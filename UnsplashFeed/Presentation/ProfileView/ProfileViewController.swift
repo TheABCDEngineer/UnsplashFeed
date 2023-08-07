49eaf6d01d50e04c8d8b20a30d7f4b1257cdbea5
TheABCDEngineer <TheABCDEngineer@yandex.ru>
2023-08-07 23:02:22 +0300

    Sprint_9

---
+++ UnsplashFeed/Presentation/ProfileView/ProfileViewController.swift
@@ -0,0 +1,121 @@
+//
+//  ProfileViewController.swift
+//  UnsplashFeed
+//
+//  Created by Avtor_103 on 06.08.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    private var userImageView: UIImageView!
+    private var userNameView: UILabel!
+    private var userLinkView: UILabel!
+    private var userSelfStatusView: UILabel!
+    @objc private var logoutButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .ypBlack
+        configureLayout()
+    }
+    
+    private func configureLayout() {
+        let userImageSupperView = UIImageView()
+        userImageSupperView.image = UIImage(systemName: "circle.fill")
+        userImageSupperView.tintColor = .ypWhite
+        userImageSupperView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(userImageSupperView)
+        userImageSupperView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        userImageSupperView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        userImageSupperView.leadingAnchor.constraint(
+            equalTo: view.leadingAnchor,
+            constant: 16
+        ).isActive = true
+        userImageSupperView.topAnchor.constraint(
+            equalTo: view.topAnchor,
+            constant: 76
+        ).isActive = true
+        
+        userImageView = UIImageView()
+        userImageView.image = UIImage(systemName: "person.crop.circle.fill")
+        userImageView.tintColor = .ypGray
+        userImageView.translatesAutoresizingMaskIntoConstraints = false
+        userImageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        userImageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        userImageSupperView.addSubview(userImageView)
+        
+        userNameView = UILabel()
+        initUserPropertyLabel(
+            label: userNameView,
+            textColor: .ypWhite,
+            font: .boldSystemFont(ofSize: 23),
+            previousLabelBottomAnchor: userImageSupperView.bottomAnchor
+        )
+        
+        userLinkView = UILabel()
+        initUserPropertyLabel(
+            label: userLinkView,
+            textColor: .ypGray,
+            font: .systemFont(ofSize: 13),
+            previousLabelBottomAnchor: userNameView.bottomAnchor
+        )
+        
+        userSelfStatusView = UILabel()
+        initUserPropertyLabel(
+            label: userSelfStatusView,
+            textColor: .ypWhite,
+            font: .systemFont(ofSize: 13),
+            previousLabelBottomAnchor: userLinkView.bottomAnchor
+        )
+        
+        logoutButton = UIButton.systemButton(
+            with: UIImage(systemName: "ipad.and.arrow.forward")!,
+            target: self,
+            action: #selector(self.onLogoutButtonClick)
+        )
+        logoutButton.tintColor = .red
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+        logoutButton.widthAnchor.constraint(equalToConstant: 20).isActive = true
+        logoutButton.heightAnchor.constraint(equalToConstant: 22).isActive = true
+        view.addSubview(logoutButton)
+        logoutButton.trailingAnchor.constraint(
+            equalTo: view.trailingAnchor,
+            constant: -24
+        ).isActive = true
+        logoutButton.centerYAnchor.constraint(
+            equalTo: userImageSupperView.centerYAnchor
+        ).isActive = true
+      
+        //будет пустым до авторизации и будет заменено реальными данными юзера после авторизации
+        userNameView.text = "Екатерина Новикова"
+        userLinkView.text = "@ekaterina_nov"
+        userSelfStatusView.text = "Hello, world!"
+        
+    }
+    
+    private func initUserPropertyLabel(
+        label: UILabel,
+        textColor: UIColor,
+        font: UIFont,
+        previousLabelBottomAnchor: NSLayoutYAxisAnchor
+    ) {
+        label.textColor = textColor
+        label.font = font
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        label.leadingAnchor.constraint(
+            equalTo: view.leadingAnchor,
+            constant: 16
+        ).isActive = true
+        label.topAnchor.constraint(
+            equalTo: previousLabelBottomAnchor,
+            constant: 8
+        ).isActive = true
+    }
+    
+    @objc
+    private func onLogoutButtonClick() {
+     
+    }
+}
---
+++ UnsplashFeed/Presentation/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,68 @@
+//
+//  SingleImageViewController.swift
+//  UnsplashFeed
+//
+//  Created by Avtor_103 on 06.08.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    var image: UIImage? {
+        didSet {
+            if isViewLoaded {
+                initImage(self.image)
+            }
+        }
+    }
+    @IBOutlet private var scrollView: UIScrollView!
+    @IBOutlet private var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.delegate = self
+        initImage(self.image)
+    }
+    
+    private func initImage(_ image: UIImage?) {
+        imageView.image = image
+        if image == nil { return }
+        configureScrollView(imageSize: image!.size)
+    }
+    
+    private func configureScrollView(imageSize: CGSize) {
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        let visibleRectSize = scrollView.bounds.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+    
+    @IBAction private func onBackButtonClick() {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func onShareButtonClick() {
+        guard let image = self.image else { return }
+        let share = UIActivityViewController(
+            activityItems: [image],
+            applicationActivities: nil
+        )
+        present(share, animated: true, completion: nil)
+    }
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+}
